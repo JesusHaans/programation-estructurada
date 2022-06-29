dd17a79fb90323476ad80259c28309703a5503d0
JesusHaans <jesuslopez@ciencias.unam.mx>
2022-06-28 21:07:48 -0500

    adding the main code and ignore some thigs of codeblocks

---
+++ MiPrimeraVariable/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main()
+{
+    // solo declaramos la variable pero no la inicializamos
+    int integerA;
+
+    //declaramos e inicializamos la variable
+    int integerB= 13;
+    float floatA= 1.1;//flotante
+    char letterA= 'A';//caracter
+    bool boolean= false;//buleano no existe por si solo necesitamos una libreria stdbool.h
+
+    return 0;
+}
